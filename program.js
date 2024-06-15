ae4b75e8a9b9710aa22d31e86c93de94a54305b4
Kayden <kayden-mcmanaway@live.nmit.ac.nz>
2024-06-16 11:47:02 +1200

    Package Install

---
+++ program.js
@@ -0,0 +1,15 @@
+// Project Health Records Management System
+
+//SECTION Variables
+import { addTestPatient } from './unitTesting.js';
+
+
+//SECTION Account Creation/Login
+
+//SECTION Health Record Display
+
+//SECTION Health Record Mutation
+
+//SECTION Save/Load Records
+
+//SECTION Menu
